bcbc4d7a161670823a0aaa188a8a80ca2b9dc845
gygever <toneev.toly@yandex.ru>
2023-06-21 00:11:07 +0400

    удаление
    был добавлен путь /delete/, который удаляет заметку, в index.html была внесена форма для создания кнопки удаления, решается проблема цикличного импорта, при нормальном расположении app и db

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from model import app, db, Tasks
-from flask import render_template
+from model import Tasks, app, db
+from flask import render_template, request
 
 
 @app.route('/')
-def hello_world():
+def show():
     tasks = Tasks.query.all()
     return render_template('index.html', tasks=tasks)
+
+
+@app.route('/add')
+def add():
+    pass
+
+
+@app.route('/delete/<int:id_del>', methods=['POST'])
+def delete(id_del):
+    task = Tasks.query.get(id_del)
+    db.session.delete(task)
+    db.session.commit()
+    tasks = Tasks.query.all()
+    return render_template('index.html', tasks=tasks)
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    pass
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -10,7 +10,3 @@ class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(30))
     content = db.Column(db.String(255))
-
-
-def __repr__(self):
-    return "<>".format(id, self.name)
