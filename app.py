7830b5bf06497303472df1c78b33889e94dd8010
gygever <toneev.toly@yandex.ru>
2023-06-22 21:30:32 +0400

    обновление app.py и model.py
    была решена проблема цикличного импорта, инициализация app  была перенесена в app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from model import Tasks, app, db
-from flask import render_template, request
+from model import Tasks, db
+from flask import render_template, request, Flask
+
+app = Flask(__name__, template_folder='template')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db.init_app(app)
 
 
 @app.route('/')
--- model.py
+++ model.py
@@ -1,9 +1,6 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, template_folder='template')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
 class Tasks(db.Model):
